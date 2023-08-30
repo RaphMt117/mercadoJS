8548084b46e046dd13471fc2e3a6e099ed8464a5
RaphMt <rafa.mtorres117@outlook.com>
2023-08-30 18:47:08 -0300

    cart menu using javascript

--- main.js
+++ main.js
@@ -1,73 +1,75 @@
+import {inicializarCarrinho} from './src/menuCarrinho';
+
 const catalogo = [
-    {
-        id: 1,
-        marca: 'Zara',
-        nome: 'Camisa Larga com Bolsos',
-        preco: 70,
-        imagem: 'product-1.jpg',
-        feminino: false,
-    },
-    {
-        id: 2,
-        marca: 'Zara',
-        nome: 'Casaco Reto com Lã',
-        preco: 85,
-        imagem: 'product-2.jpg',
-        feminino: true,
-    },
-    {
-        id: 3,
-        marca: 'Zara',
-        nome: 'Jaqueta com Efeito Camurça',
-        preco: 60,
-        imagem: 'product-3.jpg',
-        feminino: false,
-    },
-    {
-        id: 4,
-        marca: 'Zara',
-        nome: 'Sobretudo em Mescla de Lã',
-        preco: 160,
-        imagem: 'product-4.jpg',
-        feminino: false,
-    },
-    {
-        id: 5,
-        marca: 'Zara',
-        nome: 'Camisa Larga Acolchoada de Veludo Cotelê',
-        preco: 110,
-        imagem: 'product-5.jpg',
-        feminino: false,
-    },
-    {
-        id: 6,
-        marca: 'Zara',
-        nome: 'Casaco de Lã com Botões',
-        preco: 170,
-        imagem: 'product-6.jpg',
-        feminino: true,
-    },
-    {
-        id: 7,
-        marca: 'Zara',
-        nome: 'Casaco com Botões',
-        preco: 75,
-        imagem: 'product-7.jpg',
-        feminino: true,
-    },
-    {
-        id: 8,
-        marca: 'Zara',
-        nome: 'Colete Comprido com Cinto',
-        preco: 88,
-        imagem: 'product-8.jpg',
-        feminino: true,
-    },
-]
+	{
+		id: 1,
+		marca: 'Zara',
+		nome: 'Camisa Larga com Bolsos',
+		preco: 70,
+		imagem: 'product-1.jpg',
+		feminino: false,
+	},
+	{
+		id: 2,
+		marca: 'Zara',
+		nome: 'Casaco Reto com Lã',
+		preco: 85,
+		imagem: 'product-2.jpg',
+		feminino: true,
+	},
+	{
+		id: 3,
+		marca: 'Zara',
+		nome: 'Jaqueta com Efeito Camurça',
+		preco: 60,
+		imagem: 'product-3.jpg',
+		feminino: false,
+	},
+	{
+		id: 4,
+		marca: 'Zara',
+		nome: 'Sobretudo em Mescla de Lã',
+		preco: 160,
+		imagem: 'product-4.jpg',
+		feminino: false,
+	},
+	{
+		id: 5,
+		marca: 'Zara',
+		nome: 'Camisa Larga Acolchoada de Veludo Cotelê',
+		preco: 110,
+		imagem: 'product-5.jpg',
+		feminino: false,
+	},
+	{
+		id: 6,
+		marca: 'Zara',
+		nome: 'Casaco de Lã com Botões',
+		preco: 170,
+		imagem: 'product-6.jpg',
+		feminino: true,
+	},
+	{
+		id: 7,
+		marca: 'Zara',
+		nome: 'Casaco com Botões',
+		preco: 75,
+		imagem: 'product-7.jpg',
+		feminino: true,
+	},
+	{
+		id: 8,
+		marca: 'Zara',
+		nome: 'Colete Comprido com Cinto',
+		preco: 88,
+		imagem: 'product-8.jpg',
+		feminino: true,
+	},
+];
 
 for (const produtoCatalogo of catalogo) {
-    const cartaoProduto = `<div class='border-solid border-2 border-slate-300 rounded-xl w-48 col-span-1 ' id="card-produto-${produtoCatalogo.id}">
-    
+	const cartaoProduto = `<div class='border-solid border-2 border-slate-300 rounded-xl w-48 col-span-1 ' id="card-produto-${produtoCatalogo.id}">
+
     <img
     src="./assets/img/${produtoCatalogo.imagem}"
     alt="Produto ${produtoCatalogo.id} do MercadoJS."
@@ -84,9 +86,11 @@ for (const produtoCatalogo of catalogo) {
             <button class="relative border-2 border-slate-600 rounded-full px-2 bg-slate-500 hover:scale-[102%]">Adicionar</button>
             </div>
         </div>
-        
-    </div>
-`
 
-    document.getElementById('container-produto').innerHTML += cartaoProduto
+    </div>
+`;
+
+	document.getElementById('container-produto').innerHTML += cartaoProduto;
 }
+
+inicializarCarrinho();
