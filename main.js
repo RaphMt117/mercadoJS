fdb41366558943b8dea686853fea169b9e46e3c1
RaphMt <rafa.mtorres117@outlook.com>
2023-08-29 21:09:58 -0300

    tailwind first styles

--- main.js
+++ main.js
@@ -67,20 +67,20 @@ const catalogo = [
 
 for (const produtoCatalogo of catalogo) {
     const cartaoProduto = `
-    <div id="card-produto-1">
+    <div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
         <img
         src="./assets/img/${produtoCatalogo.imagem}"
         alt="Produto 1 do Magazine Hashtag."
         style="height: 300px"
         />
 
-        <p>${produtoCatalogo.marca}</p>
+        <p class='marca'>${produtoCatalogo.marca}</p>
         <p>${produtoCatalogo.nome}</p>
         <p>$${produtoCatalogo.preco}</p>
-            
+        
         <button>Adicionar</button>
     </div>
-    `
+`
 
     document.getElementById('container-produto').innerHTML += cartaoProduto
 }
